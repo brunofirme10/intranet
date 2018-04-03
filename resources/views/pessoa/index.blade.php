66d43229a0679cbad9943cab6e9d7acad2f12d23
Nayra Lopes <nayra.teixeira@redes.unb.br>
2018-04-03 17:16:12 -0300

    [pessoa][UPD] Adicionando botão de excluir na msg de sucesso ou error.

--- resources/views/pessoa/index.blade.php
+++ resources/views/pessoa/index.blade.php
@@ -8,6 +8,9 @@
                     <div class="panel-heading">Lista de pessoas</div>
                     @if (session('status'))
                         <div class="alert alert-success">
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
                             {{ session('status') }}
                         </div>
                     @endif
